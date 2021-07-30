c6e32451aced3de5771b8ece1cae2e30b7483ce0
Joe Strout <joe@strout.net>
2021-07-30 08:41:01 -0700

    Filled in the rest of the key.pressed name mappings.

--- soda/src/SdlGlue.cpp
+++ soda/src/SdlGlue.cpp
@@ -85,9 +85,13 @@ void Service() {
 	while (SDL_PollEvent(&e) != 0) {
 		if (e.type == SDL_QUIT) quit = true;
 		else if (e.type == SDL_KEYDOWN) {
-			keyDownMap.SetValue(e.key.keysym.sym, true);
+			Sint32 keyCode = e.key.keysym.sym;
+			if (keyCode == SDLK_KP_PERIOD) keyCode = SDLK_KP_DECIMAL;	// (normalize this inconsistency)
+			keyDownMap.SetValue(keyCode, true);
 		} else if (e.type == SDL_KEYUP) {
-			keyDownMap.SetValue(e.key.keysym.sym, false);
+			Sint32 keyCode = e.key.keysym.sym;
+			if (keyCode == SDLK_KP_PERIOD) keyCode = SDLK_KP_DECIMAL;	// (normalize this inconsistency)
+			keyDownMap.SetValue(keyCode, false);
 		}
 	}
 
@@ -118,7 +122,59 @@ void SetupKeyNameMap() {
 	keyNameMap.SetValue("right", SDLK_RIGHT);
 	keyNameMap.SetValue("up", SDLK_UP);
 	keyNameMap.SetValue("down", SDLK_DOWN);
-	// (etc.)
+	keyNameMap.SetValue("[1]", SDLK_KP_1);
+	keyNameMap.SetValue("[2]", SDLK_KP_2);
+	keyNameMap.SetValue("[3]", SDLK_KP_3);
+	keyNameMap.SetValue("[4]", SDLK_KP_4);
+	keyNameMap.SetValue("[5]", SDLK_KP_5);
+	keyNameMap.SetValue("[6]", SDLK_KP_6);
+	keyNameMap.SetValue("[7]", SDLK_KP_7);
+	keyNameMap.SetValue("[8]", SDLK_KP_8);
+	keyNameMap.SetValue("[9]", SDLK_KP_9);
+	keyNameMap.SetValue("[0]", SDLK_KP_0);
+	keyNameMap.SetValue("[+]", SDLK_KP_PLUS);
+	keyNameMap.SetValue("[-]", SDLK_KP_MINUS);
+	keyNameMap.SetValue("[*]", SDLK_KP_MULTIPLY);
+	keyNameMap.SetValue("[/]", SDLK_KP_DIVIDE);
+	keyNameMap.SetValue("[clear]", SDLK_KP_CLEAR);
+	keyNameMap.SetValue("[=]", SDLK_KP_EQUALS);
+	keyNameMap.SetValue("[.]", SDLK_KP_DECIMAL);	// tricky case: could be KP_PERIOD on some systems! (normalized in Service())
+	keyNameMap.SetValue("[clear]", SDLK_KP_CLEAR);
+	keyNameMap.SetValue("f1", SDLK_F1);
+	keyNameMap.SetValue("f2", SDLK_F2);
+	keyNameMap.SetValue("f3", SDLK_F3);
+	keyNameMap.SetValue("f4", SDLK_F4);
+	keyNameMap.SetValue("f5", SDLK_F5);
+	keyNameMap.SetValue("f6", SDLK_F6);
+	keyNameMap.SetValue("f7", SDLK_F7);
+	keyNameMap.SetValue("f8", SDLK_F8);
+	keyNameMap.SetValue("f9", SDLK_F9);
+	keyNameMap.SetValue("f10", SDLK_F10);
+	keyNameMap.SetValue("f11", SDLK_F11);
+	keyNameMap.SetValue("f12", SDLK_F12);
+	keyNameMap.SetValue("f13", SDLK_F13);
+	keyNameMap.SetValue("f14", SDLK_F14);
+	keyNameMap.SetValue("f15", SDLK_F15);
+	keyNameMap.SetValue("left shift", SDLK_LSHIFT);
+	keyNameMap.SetValue("right shift", SDLK_RSHIFT);
+	keyNameMap.SetValue("left ctrl", SDLK_LCTRL);
+	keyNameMap.SetValue("right ctrl", SDLK_RCTRL);
+	keyNameMap.SetValue("left alt", SDLK_LALT);
+	keyNameMap.SetValue("right alt", SDLK_RALT);
+	keyNameMap.SetValue("left cmd", SDLK_LGUI);
+	keyNameMap.SetValue("right cmd", SDLK_RGUI);
+	keyNameMap.SetValue("backspace", SDLK_BACKSPACE);
+	keyNameMap.SetValue("tab", SDLK_TAB);
+	keyNameMap.SetValue("return", SDLK_RETURN);
+	keyNameMap.SetValue("escape", SDLK_ESCAPE);
+	keyNameMap.SetValue("space", SDLK_SPACE);
+	keyNameMap.SetValue("delete", SDLK_DELETE);
+	keyNameMap.SetValue("enter", SDLK_KP_ENTER);
+	keyNameMap.SetValue("insert", SDLK_INSERT);
+	keyNameMap.SetValue("home", SDLK_HOME);
+	keyNameMap.SetValue("end", SDLK_END);
+	keyNameMap.SetValue("page up", SDLK_PAGEUP);
+	keyNameMap.SetValue("page down", SDLK_PAGEDOWN);
 }
 
 void DoSdlTest() {
