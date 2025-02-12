9f75e91bd070dc67b2eef491cf587814d30c7702
MachiaWorks <machiaworx@gmail.com>
2025-02-12 13:33:59 +0900

    Fixed division by 0

--- soda/src/PixelDisplay.cpp
+++ soda/src/PixelDisplay.cpp
@@ -87,7 +87,7 @@ static double LineSegIntersectFraction(Vector2 p1, Vector2 p2, Vector2 p3, Vecto
 	// Reference: http://paulbourke.net/geometry/lineline2d/
 	double num = (p4.x - p3.x) * (p1.y - p3.y) - (p4.y - p3.y) * (p1.x - p3.x);
 	double denom=(p4.y - p3.y) * (p2.x - p1.x) - (p4.x - p3.x) * (p2.y - p1.y);
-	if (denom == 0.0f) return 0.0f / 0.0f;
+	if (denom == 0.0f) return 0.0f ;
 	return num / denom;
 }
 
